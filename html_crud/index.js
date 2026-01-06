53eed840162665a807d799aef52c59e5fc169553
Joseph J <jangajoseph77@gmail.com>
2026-01-06 14:50:51 +0530

    updating updated

--- html_crud/index.js
+++ html_crud/index.js
@@ -1,27 +1,67 @@
-const express=require('express')
-const fs=require("fs")//file system
-const path= require("path")
-const app=express()
+const express = require("express");
+const app = express();
+const fs = require("fs");
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+const path = require("path");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(express.static(path.join(__dirname,'public')));// static files that are need to be send are put into public
-
-app.get('/form',(req,res)=>{
-    res.sendFile(__dirname +'/public/form.html' )
-})
-
-app.post('/formPost',(req,res)=>{
-    let data=JSON.parse(fs.readFileSync(__dirname + '/cse.json'))
+app.post("/formPost", (req, res) => {
+    let data = JSON.parse(fs.readFileSync(__dirname + "/cse.json"));
     data.push(req.body);
-    fs.writeFile(__dirname + '/cse.json', JSON.stringify(data),(err)=>{
-        res.send("success");
+    fs.writeFile(__dirname + "/cse.json", JSON.stringify(data), (err) => {
+        res.send("successfully added data");
+    });
+});
+
+
+//succ
+app.get("/form", (req, res) => {
+    res.sendFile(__dirname + "/public/form.html");
+});
+
+app.get("/deletebyid", (req, res) => {
+    res.sendFile(__dirname + "/public/delete.html");
+});
+
+
+app.get("/updateid", (req, res) => {
+    res.sendFile(__dirname + "/public/update.html");
+});
+
+
+app.post("/formdelete", (req, res) => {
+    let id = req.body.id;
+    let data = JSON.parse(fs.readFileSync(__dirname + "/cse.json"));
+    let index = data.findIndex((el) => el.id == id);
+
+    if (index !== -1)// data present
+        data.splice(index, 1)
+    else res.send("cant find id")
+    fs.writeFile(__dirname + "/cse.json", JSON.stringify(data), (err) => {
+        res.send("delted data");
     })
 })
 
 
-app.listen(3000,()=>{
-    console.log("server runnning on port 3000 ");
-})
+//update by id 
+//issue
+app.post("/rocks", (req, res) => {
 
+    let data = JSON.parse(fs.readFileSync(__dirname + "/cse.json"));
+    let field = data.find((el) => el.id);
+
+    field.name = req.body.name;
+    field.city = req.body.city;
+
+
+
+    fs.writeFile(__dirname + "/cse.json", JSON.stringify(data), () => {
+        res.send("updated dataa");
+    });
+
+});
+
+app.listen(3000, () => {
+    console.log("server runnning....");
+});
